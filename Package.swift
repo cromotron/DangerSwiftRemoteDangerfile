a6ba721456f0f1930a572ad568df53a51089dfce
William Wallace <bill@cromotron.com>
2025-03-30 14:07:38 -0700

    test

--- Package.swift
+++ Package.swift
@@ -1,37 +1,47 @@
-// swift-tools-version: 6.0
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+let isDevelop = true
+
+let developProducts: [Product] = isDevelop ? [
+    .library(
+        name: "DangerDeps",
+        type: .dynamic,
+        targets: ["DangerDependencies"]),
+] : []
+
+let developTargets: [Target] = isDevelop ? [
+    .testTarget(
+        name: "DangerSwiftRemoteDangerfileTests",
+        dependencies: ["DangerSwiftRemoteDangerfile"]),
+    .target(
+        name: "DangerDependencies",
+        dependencies: ["DangerSwiftRemoteDangerfile"]),
+] : []
+
 let package = Package(
     name: "DangerSwiftRemoteDangerfile",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v13),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(name: "DangerSwiftRemoteDangerfile", targets: ["DangerSwiftRemoteDangerfile"]),
-        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]) // dev
-    ],
+        .library(
+            name: "DangerSwiftRemoteDangerfile",
+            targets: ["DangerSwiftRemoteDangerfile"]),
+    ] + developProducts,
     dependencies: [
-            .package(url: "https://github.com/danger/swift.git", from: "3.0.0"),
-            .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.3.0"),
-            .package(url: "https://github.com/f-meloni/Rocket", from: "1.0.0"), // dev,
-        ],
+        .package(url: "https://github.com/danger/swift.git", from: "3.0.0"),
+        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.3.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "DangerDependencies", dependencies: [.product(name: "Danger", package: "swift"), "DangerSwiftRemoteDangerfile"]), // dev
         .target(
             name: "DangerSwiftRemoteDangerfile",
             dependencies: [
                 .product(name: "Danger", package: "swift"),
                 .product(name: "ShellOut", package: "ShellOut"),
-            ]
-        ),
-        .testTarget(
-            name: "DangerSwiftRemoteDangerfileTests",
-            dependencies: ["DangerSwiftRemoteDangerfile"]
-        ),
-    ]
+            ]),
+    ] + developTargets
 )
+
