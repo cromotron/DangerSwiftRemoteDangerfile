854aa959fca6fd1d09251150939fc261486824e6
William Wallace <bill@cromotron.com>
2025-03-30 14:13:30 -0700

    dangerfile

---
+++ Dangerfile.swift
@@ -0,0 +1,11 @@
+//
+//  Dangerfile.swift
+//  DangerSwiftRemoteDangerfile
+//
+//  Created by William Wallace on 3/30/25.
+//
+
+import Danger
+import DangerSwiftRemoteDangerfile
+
+DangerSwiftRemoteDangerfile().checkForCopyrightHeaders()
